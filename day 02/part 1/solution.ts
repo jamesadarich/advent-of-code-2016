9f8d7db0be3f8682ea62bc7181f0d7a634fd8f32
jamesrichford <jamesrichford@googlemail.com>
2016-12-05 08:45:45 +0000

    day 1 and day 2 updates

--- day 02/part 1/solution.ts
+++ day 02/part 1/solution.ts
@@ -1,37 +1,46 @@
+const keypad = [
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ]
+];
+
 export default class Solution {
     public solve(stringstructions: string) {
         const instructions = stringstructions.split("\n");
 
         let code = "";
         let currentValue = 5;
+        let currentPosition = {
+            x: 1, y: 1
+        };
 
         instructions.forEach(instruction => {
 
             for (let i = 0; i < instruction.length; i++) {
                 if (instruction[i] === "U") {
-                    if (currentValue > 3) {
-                        currentValue -= 3;
+                    if (keypad[currentPosition.y - 1]) {
+                        currentPosition.y--;
                     }
                 }
                 else if (instruction[i] === "D") {
-                    if (currentValue < 7) {
-                        currentValue += 3;
+                    if (keypad[currentPosition.y + 1]) {
+                        currentPosition.y++;
                     }
                 }
                 else if (instruction[i] === "R") {
-                    if (currentValue % 3 !== 0) {
-                        currentValue++;
+                    if (keypad[currentPosition.y][currentPosition.x + 1]) {
+                        currentPosition.x++;
                     }
                 }
                 else if (instruction[i] === "L") {
-                    if (currentValue % 3 !== 1) {
-                        currentValue--;
+                    if (keypad[currentPosition.y][currentPosition.x - 1]) {
+                        currentPosition.x--;
                     }
                 }
 
             }
 
-            code += currentValue.toString();
+            code += keypad[currentPosition.y][currentPosition.x].toString();
         });
 
         return code;
--- day 02/part 2/solution.ts
+++ day 02/part 2/solution.ts
@@ -1,3 +1,54 @@
+const keypad = [
+    [    ,    , "1",    ,     ],
+    [    , "2", "3", "4",     ],
+    [ "5", "6", "7", "8", "9" ],
+    [    , "A", "B", "C",     ],
+    [    ,    , "D",    ,     ]
+];
+
 export default class Solution {
-    
+    public solve(stringstructions: string) {
+        const instructions = stringstructions.split("\n");
+
+        let code = "";
+        let currentValue = 5;
+        let currentPosition = {
+            x: 0, y: 2
+        };
+
+        instructions.forEach(instruction => {
+
+            for (let i = 0; i < instruction.length; i++) {
+                if (instruction[i] === "U") {
+                    if (keypad[currentPosition.y - 1] && keypad[currentPosition.y - 1][currentPosition.x]) {
+                        currentPosition.y--;
+                    }
+                }
+                else if (instruction[i] === "D") {
+                    if (keypad[currentPosition.y + 1] && keypad[currentPosition.y + 1][currentPosition.x]) {
+                        currentPosition.y++;
+                    }
+                }
+                else if (instruction[i] === "R") {
+                    if (keypad[currentPosition.y][currentPosition.x + 1]) {
+                        currentPosition.x++;
+                    }
+                }
+                else if (instruction[i] === "L") {
+                    if (keypad[currentPosition.y][currentPosition.x - 1]) {
+                        currentPosition.x--;
+                    }
+                }
+
+            }
+
+            try {
+            code += keypad[currentPosition.y][currentPosition.x].toString();}
+            catch(error) {
+                throw new Error(JSON.stringify(currentPosition));
+            }
+        });
+
+        return code;
+    }
 }
